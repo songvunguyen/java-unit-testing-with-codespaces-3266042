3dc038838809a3261379d6ba8cd659b9ebff652a
Song Vu Nguyen <38381350+songvunguyen@users.noreply.github.com>
2023-12-11 17:13:53 +0000

    Solved problems

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -15,4 +15,31 @@ public class CalculatorTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void testSubtract() {
+        int expected = 1;
+
+        int actual = calculator.subtract(2, 1);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMultiply() {
+        int expected = 6;
+
+        int actual = calculator.multiply(2, 3);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testDivide() {
+        int expected = 3;
+
+        int actual = calculator.divide(3, 1);
+
+        assertEquals(expected, actual);
+    }
+
 }
